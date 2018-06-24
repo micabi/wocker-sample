e205cb937975c44c704ea4fc21c1797f626ef534
My Ubuntu <w.adachi1127@gmail.com>
2018-06-24 23:56:15 +0900

    更新　

--- wp-content/plugins/my-custom-admin/my-custom-admin.php
+++ wp-content/plugins/my-custom-admin/my-custom-admin.php
@@ -32,8 +32,8 @@ function my_admin_menu(){
 	);
 
   add_options_page(
-	'フォームを追加',
-	'フォームを追加',
+	'フォームを追加するプラグイン',
+	'フォームを追加するプラグイン',
 	'administrator',
 	'add_my_form',
 	'add_my_form'
@@ -59,10 +59,10 @@ function my_custom_submenu(){
 function add_my_form(){
   ?>
 	<div class="wrap">
-		<h2>フォームを追加</h2>
+		<h2>設定に「フォームを追加する」プラグイン</h2>
 		<form action="" method="POST">
 			<?php wp_nonce_field('my-nonce', 'add_my_form'); ?><!-- CSRF対策 -->
-			<p><input type="text" name="text-data" value="<?php echo esc_attr( get_option('text-data') ); ?>"></p>
+			<p><label for="e-mail">E-mail Address: <input type="text" name="text-data" value="<?php echo esc_attr( get_option('text-data') ); ?>" id="e-mail"></label></p>
 			<p><input type="submit" value="保存" class="button button-primary button-large"></p>
 		</form>
 	</div><!-- /.wrap -->
@@ -71,11 +71,37 @@ function add_my_form(){
 
 add_action('admin_init', 'my_admin_init'); // フォームからの送信内容を保存
 function my_admin_init(){
-	if( isset( $_POST['add_my_form'] ) && $_POST['add_my_form'] ){
-		if( check_admin_referer( 'my-nonce', 'add_my_form' ) ){
-			var_dump($_POST['text-data']);
+	if( !empty( $_POST ) && check_admin_referer( 'my-nonce', 'add_my_form' ) ){
+		//var_dump($_POST['text-data']);
+
+		if( isset( $_POST['text-data'] ) && $_POST['text-data'] ){
+			$e = new WP_Error();
+			if( is_email( trim( $_POST['text-data'] ) ) ){
+				update_option( 'text-data', trim( $_POST['text-data'] ) );
+			}else{
+				$e->add( 'error', 'メールアドレスの形式ではありません。');
+				set_transient( 'add-my-form-error', $e->get_error_messages(), 10 );
+			}
+		}else{
+			update_option( 'text-data', '' );
 		}
 
+		wp_safe_redirect( menu_page_url( 'add_my_form' ), false );
 	}
+}
+
+add_action('admin_notices', 'my_admin_notices');
+function my_admin_notices(){
+?>
+	<?php if( $messages = get_transient( 'add-my-form-error' ) ): ?>
+	<div class="error">
+		<ul>
+			<?php foreach( $messages as $message ): ?>
+			<li><?php echo esc_html( $message ); ?></li>
+			<?php endforeach; ?>
+		</ul>
+	</div><!-- /.error -->
+	<?php endif; ?>
+<?php
 }
 
